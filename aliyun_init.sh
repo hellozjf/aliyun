2dc2194de33024a170f9c8e1d7f9cbc3337f19e3
Jingfeng Zhou <908686171@qq.com>
2018-07-24 20:55:54 +0800

    update

--- aliyun_init.sh
+++ aliyun_init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# 初始化阿里云
 
 # 1. 配置基础信息
 wget https://raw.githubusercontent.com/hellozjf/aliyun/master/aliyun_system.sh && chmod +x aliyun_system.sh && bash aliyun_system.sh
@@ -9,5 +10,8 @@ wget https://raw.githubusercontent.com/hellozjf/aliyun/master/aliyun_ipv6.sh && 
 # 3. 安装docker
 wget https://raw.githubusercontent.com/hellozjf/aliyun/master/aliyun_docker.sh && chmod +x aliyun_docker.sh && bash aliyun_docker.sh
 
-# 4. 重启系统
+# 4. 清除所有无用的文件
+rm -rf *
+
+# 5. 重启系统
 reboot
